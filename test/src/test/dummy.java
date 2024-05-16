5dfa3a671fa2206e57e61312026170031defd387
divyabharathi.mathav <divyabharathi.mathav@DIVYA-FCLZKR3>
2024-05-16 12:57:32 +0530

    test

--- test/src/test/dummy.java
+++ test/src/test/dummy.java
@@ -5,7 +5,7 @@ public class dummy {
 	public static void main(String[] args) {
 		System.out.println("Dummy program by Bharath");
 		System.out.println("Welcome");
-		System.out.println("back push6575765");
+		System.out.println("------------");
 
 	}
 
